22042be24cd59b2e8f3cb724ce78065b5c630eee
filipe <filipe.barbosa@bs2tecnologia.com.br>
2022-02-04 09:21:07 -0300

    Creat interface for data source

--- core/src/main/java/com/example/core/data/repository/CharactersRepository.kt
+++ core/src/main/java/com/example/core/data/repository/CharactersRepository.kt
@@ -1,6 +1,9 @@
 package com.example.core.data.repository
 
+import androidx.paging.PagingSource
+import javax.xml.stream.events.Characters
+
 interface CharactersRepository {
 
-    fun getCharacters(query: String): PagingSource
+    fun getCharacters(query: String): PagingSource< Int, Characters >
 }
